6b80b097b677164c50a2ad5722a00f80dd626955
SusheelSatyamsetty98 <satyamsettysusheel98@gmail.com>
2025-05-04 22:51:53 +0530

    longestValidParentheses temp in 1981 file.

--- Susheel/Medium/1981_MinDifferenceBtwTargetChosenElements/temp.cpp
+++ Susheel/Medium/1981_MinDifferenceBtwTargetChosenElements/temp.cpp
@@ -2,58 +2,48 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
 class Solution {
 public:
-    int allPossibleSetCount = 0;
-    std::vector<int> dp_vec;
-    int helpFn(int start,vector<int>& mat,int k)
-    {
-        int res = 0;
-        int temp = start;
-        int itr = start + 1;
-
-        while(temp < mat.size() - 1)
-        {
-            if (itr > mat.size()) return res;
-            
-            if(abs(mat[start] - mat[itr++]) != k)
-            {
-                res++;
-            }
-            temp++;
-        }
-
-        return res;
-    }
-    
-    int findAllPossibleSubsets()
-    {
-        return allPossibleSetCount;
-    }
-    
-    int beautifulSubsets(vector<int>& nums, int k)
-    {
-        dp_vec.resize(nums.size());
-        allPossibleSetCount += nums.size();
-        sort(nums.begin(), nums.end());
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        map<pair<int,int>,int> dp;
+        int i = 0;
         
-        for(int i = 0; i < nums.size() - 1; i++)
+        while(i <= s.size())
         {
-            dp_vec[i] = helpFn(i,nums,k);
-            allPossibleSetCount += dp_vec[i];
-            cout << i << " " << dp_vec[i] << " " << allPossibleSetCount << endl;
+            if(s[i] == '(')
+            {
+                st.push(i);
+            }
+            else if(s[i] == ')')
+            {
+                if(!st.empty())
+                {
+                    dp[{i,st.top()}] = (i - st.top())+1);
+                    st.pop();
+                }
+            }
+            i++;
         }
-        exit(1);
-        return findAllPossibleSubsets();
+        
+        for(auto it = dp.begin(), it != dp.end(); ++it)
+        {
+            pair<int,int> first_p = it->first;
+            int  first_c = it->second;
+            //Write the logic here
+        }
+        return count;
     }
 };
 
-int main() {
+int main()
+{
     Solution sol;
-    int res = 0;
-    vector<int> nums = {1,2,4,3,5};
-    int k = 1;
-    
-    res = sol.beautifulSubsets(nums,k);
+    string temp = "(()";
+
+    int res = sol.longestValidParentheses(temp);
+ 
+    cout << "Longest substring is : " << res << endl;
     return 0;
 }
